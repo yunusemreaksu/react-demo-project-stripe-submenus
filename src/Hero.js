9c319ae6afd32f1eb615bde542c6db71930488d5
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-18 10:29:18 +0300

    Added context boilerplate

--- src/Hero.js
+++ src/Hero.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-
+import { useGlobalContext } from './context'
 const Hero = () => {
+  const data = useGlobalContext();
   return <h2>hero component</h2>
 }
 
