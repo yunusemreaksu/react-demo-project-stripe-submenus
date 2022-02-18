f04436503837f0853dadd037b9c526a824d61e4d
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-18 14:34:30 +0300

    Added Hero component's functions

--- src/Hero.js
+++ src/Hero.js
@@ -2,8 +2,21 @@ import React from 'react'
 import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 const Hero = () => {
-  const data = useGlobalContext();
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Paymenets infrastructure for the internet</h1>
+          <p>Some Copy-Paste Paragraph</p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className="phone-img" alt='phone'/>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
